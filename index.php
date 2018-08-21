917c09de8a59982c6a7b3ced85ce672f9311384c
Estevam Morais <estevamferretti@gmail.com>
2018-08-21 15:20:46 -0300

    PTL ADMIN

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("vendor/autoload.php");
 // Config
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -14,6 +15,14 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
