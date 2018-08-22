804bff0f6bca4432d0ac9d3d0a28b0e58787104b
Estevam Morais <estevamferretti@gmail.com>
2018-08-22 10:29:20 -0300

    VIEW LOGIN

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 // Config
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
 // Content Important
+// Strore
 $app->get('/', function() {
     
 	$page = new Page();
@@ -20,11 +22,44 @@ $app->get('/', function() {
 
 });
 
+// Admin
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+// Login Admin (GET)
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+// Login Admin (POST)
+$app->post('/admin/login', function() {
+
+	User::Login($_POST["login"], $_POST["password"]);
+
+	header("location: /admin");
+	exit;
+
+});
+// Logout
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("location: /admin/login");
+	exit;
 
 });
 
