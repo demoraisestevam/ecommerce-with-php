46f7f3e3a62c3e34e2ab1cbeedb909526708d224
Estevam Morais <estevamferretti@gmail.com>
2018-09-02 17:43:05 -0300

    SITE CATEGORIES

--- index.php
+++ index.php
@@ -325,6 +325,20 @@ $app->post("/admin/categories/:idcategory", function($idcategory) {
 
 });
 
+$app->get("/category/:idcategory", function($idcategory) {
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", array(
+		'category'=>$category->getValues(),
+		'products'=>[]
+	));
+
+});
 
 // Run Application
 $app->run();
