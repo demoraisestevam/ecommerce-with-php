deadb01237ebb167be77a7031d10d3c9fa541d97
Estevam Morais <estevamferretti@gmail.com>
2018-09-05 00:13:43 -0300

    SITE --PRODUCTS

--- site.php
+++ site.php
@@ -1,13 +1,18 @@
 <?php 
 
 use \Hcode\Page;
+use \Hcode\Model\Product;
 
 // Strore
 $app->get('/', function() {
     
+	$products = Product::listAll();
+
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", array(
+		"products"=>Product::checkList($products)
+	));
 
 });
 
